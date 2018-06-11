90248c8ad92096e92ff8b044bf6a727deaf848fb
zacksleo <zacksleo@gmail.com>
2018-06-11 11:21:11 +0800

    update config

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,5 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@tests', dirname(dirname(__DIR__)) . '/tests');
+(new \Dotenv\Dotenv(dirname(dirname(__DIR__))))->load();
