6f0448e58125076ef7cd111ee39fce7ba8f312e7
zacksleo <zacksleo@gmail.com>
2018-11-08 14:16:49 +0800

    fix(env): invalid cookieValidationKey

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -3,7 +3,7 @@
 $config = [
     'components' => [
         'request' => [
-            'cookieValidationKey' => 'DummyValidationKey',
+            'cookieValidationKey' => getenv('COOKIE_VALIDATION_KEY')
         ],
     ],
 ];
