ee3b849f5021c14843b41a98c71066e9392831b0
zacksleo <zacksleo@gmail.com>
2018-11-08 11:39:30 +0800

    chore(forms): remove useless forms

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -8,7 +8,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
+use frontend\widgets\Alert;
 
 AppAsset::register($this);
 ?>
